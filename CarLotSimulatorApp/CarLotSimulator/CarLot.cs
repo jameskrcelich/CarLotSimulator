b96eb3b03b2d06aa8277ebd06e6c74999976cd38
James Krcelich <james.krcelich@sbcglobal.net>
2025-10-28 18:34:55 -0500

    add new files

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace CarLotSimulator;
+
+public class CarLot
+{
+        public CarLot() {}
+
+        public List<Car> ParkingLot = new List<Car>();
+}
+  
